ebdf9fa12084e5273d25d73d61807246fd5c51ca
Iwan Dwi Prakoso <iwan.dprakoso@gmail.com>
2021-06-01 10:08:14 +0700

    add more assets

--- crowns/lib/screens/auth/login.dart
+++ crowns/lib/screens/auth/login.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 
+import 'package:crowns/utils/image_constants.dart';
+
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        height: double.infinity,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [Image.asset(ImageConstants.appLogo)],
+          ),
+        ),
+      ),
+    );
   }
 }
--- crowns/lib/utils/image_constants.dart
+++ crowns/lib/utils/image_constants.dart
@@ -1,3 +1,22 @@
 class ImageConstants {
-  static String logo = 'assets/images/logo_dummy.png';
+  static String appLogo = 'assets/images/logo-app.png';
+  static String homeHeader = 'assets/images/header-home.png';
+  static String appHeader = 'assets/images/header-app.png';
+  static String jahitSeakarangBanner =
+      'assets/images/banner-jahit-sekarang.png';
+  static String kreasiDisplay1 = 'assets/images/display-kreasi-1.png';
+  static String terdekatDisplay1 = 'assets/images/display-terdekat-1.png';
+  static String bankBCAlogo = 'assets/images/logo-bank-bca.png';
+  static String bankBNIlogo = 'assets/images/logo-bank-bni.png';
+  static String bankBRILogo = 'assets/images/logo-bank-bri.png';
+  static String bankMandiriLogo = 'assets/images/logo-bank-mandiri.png';
+  static String facebookLogo = 'assets/images/logo-facebook.png';
+  static String googleLogo = 'assets/images/logo-google.png';
+  static String formalProduct1 = 'assets/images/product-formal-1.png';
+  static String pestaProduct1 = 'assets/images/product-pesta-1.png';
+  static String progressBar1 = 'assets/images/progress-bar-1.png';
+  static String progressBar2 = 'assets/images/progress-bar-2.png';
+  static String progressBar3 = 'assets/images/progress-bar-3.png';
+  static String progressBar4 = 'assets/images/progress-bar-4.png';
+  static String splashScreen = 'assets/images/spalsh-screen.png';
 }
