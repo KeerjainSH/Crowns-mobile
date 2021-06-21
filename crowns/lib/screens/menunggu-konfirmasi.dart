5edad503d58aa3dbb1452d07a6a72d08146af1fa
Iwan Dwi Prakoso <iwan.dprakoso@gmail.com>
2021-06-21 20:49:59 +0700

    feat: edit view menunuggu konfirmasi

--- crowns/lib/screens/menunggu-konfirmasi.dart
+++ crowns/lib/screens/menunggu-konfirmasi.dart
@@ -9,13 +9,22 @@ class MenungguKonfirmasiScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var padding = MediaQuery.of(context).padding;
 
-    final content = Text(
-      'Menunggu Konfirmasi Pembayaran',
-      style: TextStyle(
-        color: ColorConstants.darkGrey,
-        fontWeight: FontWeight.w400,
-        fontSize: 16,
-      ),
+    final content = Column(
+      children: [
+        Text(
+          'Terimakasih sudah melakukan pesanan',
+          style: TextStyle(
+            // color: ColorConstants.bla,
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        Text(
+          'Kamu bisa melihat status pesanan pada menu cart',
+          style: TextStyle(color: ColorConstants.darkGrey),
+        ),
+      ],
     );
 
     return SafeArea(
@@ -23,6 +32,7 @@ class MenungguKonfirmasiScreen extends StatelessWidget {
         body: Container(
           height:
               MediaQuery.of(context).size.height - padding.top - padding.bottom,
+          padding: EdgeInsets.symmetric(horizontal: 32),
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -35,7 +45,14 @@ class MenungguKonfirmasiScreen extends StatelessWidget {
                 SizedBox(height: 25),
                 content,
                 SizedBox(height: 25),
-                buildButton(context, 'Kembali', RouteConstants.landingPage),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    buildButton(context, 'Kembali', RouteConstants.landingPage),
+                    SizedBox(width: 20),
+                    buildButton(context, 'Cart', RouteConstants.landingPage),
+                  ],
+                ),
                 SizedBox(height: 100),
               ],
             ),
